52656a7bc859695755f70b4188e766cfd2595147
Glitch <glitch.txs@gmail.com>
2024-02-08 18:53:06 -0300

    add headers

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  server: {
-    headers: {
-      'Content-Security-Policy': `default-src 'self'; connect-src 'self';`,
-    },
-  },
-})
