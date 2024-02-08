99f9889b0b2dbbc3c6820a9a161793a637f6eb7e
Glitch <glitch.txs@gmail.com>
2024-02-08 20:18:06 -0300

    simplify

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,20 @@
-import { WindowPostMessageStream } from '@metamask/post-message-stream';
+import { WindowPostMessageStream } from '@metamask/post-message-stream'
 import { initializeProvider } from '@metamask/providers'
 
 // MetaMask injection hack
 // Due to https://github.com/MetaMask/metamask-extension/issues/3133
 
-(() => {
-  if (window.ethereum) {
-    return;
-  }
-  if (navigator.userAgent.includes('Firefox')) {
-    // setup background connection
-    const metamaskStream = new WindowPostMessageStream({
-      name: 'metamask-inpage',
-      target: 'metamask-contentscript'
-    });
+if (
+  typeof window !== 'undefined' && 
+  //@ts-expect-error
+  !window.ethereum && 
+  navigator.userAgent.includes('Firefox')) {
+  // setup background connection
+  const connectionStream = new WindowPostMessageStream({
+    name: 'metamask-inpage',
+    target: 'metamask-contentscript'
+  })
 
-    // this will initialize the provider and set it as window.ethereum
-    initializeProvider({
-      connectionStream: metamaskStream,
-      shouldShimWeb3: true
-    });
-  }
-})();
-
-declare global {
-  interface Window {
-    ethereum: unknown
-  }
+  // this will initialize the provider and set it as window.ethereum
+  initializeProvider({ connectionStream })
 }
