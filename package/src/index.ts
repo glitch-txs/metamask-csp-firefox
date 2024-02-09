e9c44ea0a8779ec9a35a08594d6be70df0e63edf
Glitch <glitch.txs@gmail.com>
2024-02-08 21:41:06 -0300

    add workaround

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,5 +1,6 @@
 // MetaMask injection hack
 // Due to https://github.com/MetaMask/metamask-extension/issues/3133
+
 (async ()=>{
   if (
     typeof window !== 'undefined' && 
@@ -18,6 +19,26 @@
   
     // this will initialize the provider and set it as window.ethereum
     initializeProvider({ connectionStream })
+
+    //@ts-expect-error
+    const ethereum = window.ethereum
+
+    // Workaround if MetaMask is not installed
+    if(ethereum){
+      const timeOut = new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      })
+
+      const isInstalled = await Promise.race([
+        timeOut, 
+        // MetaMask always return the chainId even when the wallet is locked - unstable
+        ethereum.request({ method: 'eth_chainId' })
+      ])
+      if(!isInstalled){
+        //@ts-expect-error
+        window.ethereum = undefined
+      }
+    }
   }
 })()
 
